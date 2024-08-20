2411dc135deb68deb2dceb06f29410c2a682e379
Andidela <andidela83@gmail.com>
2024-08-20 14:21:41 +0000

    Proyecto Finalizado

---
+++ app.js
@@ -0,0 +1,32 @@
+// We declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar;
+}
+// We declare the function with the exact name "fromDollarToYen"
+const fromDollarToYen = function(valueInDollar) {
+    // Convert the given valueInEuro to dollars
+    let valueInYen = valueInDollar * 146.26;
+    // return the dollar value
+    return valueInDollar;
+}
+// We declare the function with the exact name "fromYenToPound"
+const fromYenToPound = function(valueInYen) {
+    // Convert the given valueInEuro to dollars
+    let valueInPounds = valueInYen / 179.88;
+    // return the dollar value
+    return valueInPounds;
+}
+// This is my function that sums two numbers
+const sum = (a,b) => {
+    return a + b
+}
+
+// Just a console log for ourselves
+console.log(sum(7,3))
+
+// Export the function to be used on other files 
+// (similar to the keyword "export" when using webpack)
+module.exports = { sum, fromEuroToDollar };
